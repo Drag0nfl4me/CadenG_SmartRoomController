5e4dc9a810ce62e68173b52b6d6000633b941e18
Drag0nfl4me <cgamache1@cnm.edu>
2023-03-09 18:00:39 -0700

    added wemos and a bunch of comments explaining most if not all components

--- 3PawnGame_RoomController/src/3PawnGame_RoomController.ino
+++ 3PawnGame_RoomController/src/3PawnGame_RoomController.ino
@@ -1,8 +1,8 @@
 /*
  * Project 3PawnGame_RoomController
- * Description: A smart room controller that reads an active 3-pawn game to control lights and wemos
+ * Description: A smart room controller that reads an active 3-pawn game to control hue lights and wemos
  * Author: Caden Gamache
- * Date: 03/01/2023
+ * Date: 03/01/2023 - 03/09/2023
  */
 
 SYSTEM_MODE(SEMI_AUTOMATIC);
@@ -11,18 +11,20 @@ SYSTEM_MODE(SEMI_AUTOMATIC);
 #include <Adafruit_SSD1306.h>
 #include <neopixel.h>
 
-float nominalRead [3][3];
-float actualRead [3][3];
+float actualRead [3][3];  //untouched array storing the volages read
+float nominalRead [3][3]; //rounded array based on actual read
+//the cordinates for the center of every square. x (even columns 0,2,4) and y (odd columns 1,3,5)
 int piecePos [3][6] {
   {15, 48, 15, 31, 15, 15},
   {31, 48, 31, 31, 31, 15},
   {48, 48, 48, 31, 48, 15}
 };
-int i,j,k,currentRow ,currentColor, currentAngle;
-int redPiece,bluePiece;
-int neopixNum;
-const int COLORS [] {0,62309,59086,53714,48343,45120,42971};
-const int NEOCOLORS [] {0xFF0000,0xD5002B,0xAA0055,0x800080,0x5500AA,0x2B00D5,0x0000FF};
+const int COLORS [] {0,62309,59086,53714,48343,45120,42971}; //gradient from red to blue (HSV)
+const int NEOCOLORS [] {0xFF0000,0xD5002B,0xAA0055,0x800080,0x5500AA,0x2B00D5,0x0000FF}; //gradient from red to blue (RGB)
+int i,j,k;
+int currentRow,currentColor; //variables called in multiple functions
+int redPiece,bluePiece; //stores the number of red (1's) or blue (2's) pieces
+int wemo1 = 2,wemo2 = 3;
 
 void checkPiecePos ();
 void rowSetup ();
@@ -33,11 +35,13 @@ void drawPieces();
 Adafruit_NeoPixel neopix(12,A4,WS2812B);
 Adafruit_SSD1306 display(D2);
 
+
 void setup() {
 
   // WiFi.on();
   // WiFi.setCredentials("IoTNetwork");
   // WiFi.connect();
+  // //forces the system to wait for the wifi to connect
   // while(WiFi.connecting()) {
   // delay(1);
   // }
@@ -66,58 +70,65 @@ void loop() {
   drawBoard();
   drawPieces();
   display.display();
-  //victory
+  //adjusts hues if either side achieves victory
   if (nominalRead[0][0] == 2 || nominalRead[0][1] == 2 || nominalRead[0][2] == 2) {
-    setHue(3,true,COLORS[6],255,255);
+    for (i=0;i<7;i++) {
+      setHue(i,true,COLORS[6],255,255);
+    }
     pixelFill(0,12,NEOCOLORS[6]);
+    switchON(wemo1);
     delay(5000);
   }
   if (nominalRead[2][0] == 1 || nominalRead[2][1] == 1 || nominalRead[2][2] == 1) {
-    setHue(3,true,COLORS[0],255,255);
+    for (i=0;i<7;i++) {
+      setHue(i,true,COLORS[0],255,255);
+    }
     pixelFill(0,12,NEOCOLORS[0]);
+    switchON(wemo2);
     delay(5000);
   }
-  //on-going
+  //on-going (adjust hues based on the active game)
   else {
-    setHue(3,true,COLORS[currentColor],100,255);
+    for (i=0;i<7;i++) {
+      setHue(i,true,COLORS[currentColor],100,255);
+    }
+    switchOFF(wemo1);
+    switchOFF(wemo2);
     pixelFill(0,12,NEOCOLORS[currentColor]);
   }
 }
-
+//looks through the nominal read and adds a red or blue piece if the values match
 void determineAdvantage () {
   //reset values
   redPiece = 0;
   bluePiece = 0;
-  //read number of red and blue pieces
+  //read the number of red and blue pieces
   for (j=0;j<3;j++) {
     for (i=0;i<3;i++) {
       if (nominalRead[j][i] == 1) {redPiece ++;}
       if (nominalRead[j][i] == 2) {bluePiece ++;}
     }
   }
-  //adjust colors and servo angle
+  //adjust colors
   if (redPiece > bluePiece) {
     currentColor = 3 - (redPiece - bluePiece); 
-    currentAngle = 3 - (redPiece - bluePiece);
   }
   if (redPiece < bluePiece) {
     currentColor = 3 + (bluePiece - redPiece);
-    currentAngle = 3 + (bluePiece - redPiece);
   }
   if (redPiece == bluePiece) {
     currentColor = 3;
-    currentAngle = 3;
   }
   Serial.printf("\nNumRed = %i NumBlue = %i",redPiece,bluePiece);
 }
-
+//reads the voltage of every square and stores them in an array. It then converts the stored floats into 3 integers
 void checkPiecePos () {
   //reads all piece positions and determines side
   for (j=0;j<3;j++) {
     currentRow = j;
     rowSetup ();
     for (i=0;i<3;i++) {
-      nominalRead [i][j] = analogRead(i+17)/783.0;
+      nominalRead [i][j] = analogRead(i+17)/723.0;
       actualRead [i][j] = nominalRead [i][j];
       //converts to three values
       if (nominalRead [i][j] > 1 && nominalRead [i][j] < 2) {nominalRead [i][j] = 1;}
@@ -125,6 +136,7 @@ void checkPiecePos () {
       if (nominalRead [i][j] < 1) {nominalRead [i][j] = 0;}
     }
   }
+  //these for loops flip the array values for some clarity when reading the serial monitor
   for (i=2;i>=0;i--) {
     Serial.printf("\ncol1 = %.2f col2 = %.2f col3 = %.2f",actualRead[i][0],actualRead[i][1],actualRead[i][2]);
   }
@@ -132,8 +144,9 @@ void checkPiecePos () {
     Serial.printf("\nPieceSide = %.2f PieceSide = %.2f PieceSide = %.2f",nominalRead[i][0],nominalRead[i][1],nominalRead[i][2]);
   }
 }
-
+//switches the D pins between input/output and high/low turning one on at a time (setting to low is required for consistent values)
 void rowSetup () { 
+  //first column
   if (currentRow == 0) {
     pinMode(D8,OUTPUT);
     pinMode(D7,INPUT);
@@ -142,6 +155,7 @@ void rowSetup () {
     digitalWrite(D7,LOW);
     digitalWrite(D6,LOW);
   }
+  //second column
   if (currentRow == 1) { 
     pinMode(D8,INPUT);
     pinMode(D7,OUTPUT);
@@ -150,6 +164,7 @@ void rowSetup () {
     digitalWrite(D7,HIGH);
     digitalWrite(D6,LOW); 
   }
+  //third column
   if (currentRow == 2) {
     pinMode(D8,INPUT);
     pinMode(D7,INPUT);
@@ -159,7 +174,7 @@ void rowSetup () {
     digitalWrite(D6,HIGH);
   }
 }
-
+//resets the board on the dysplay (without showing changes)
 void drawBoard () {
   display.clearDisplay();
   //left edge
@@ -175,7 +190,7 @@ void drawBoard () {
   display.fillRect(7,24,16,16,WHITE);
   display.fillRect(41,24,16,16,WHITE);
   display.fillRect(24,41,16,16,WHITE);
-
+  //text and symbols
   display.setTextColor(WHITE);
   display.setTextSize(1);
   display.setCursor(68,16);
@@ -185,36 +200,35 @@ void drawBoard () {
   display.fillCircle(118,18,4,WHITE);
   display.fillTriangle(118,41,114,49,122,49,WHITE);
 }
-
+//reads the nominal array and puts a circle or triangle in the correct square
 void drawPieces () {
+  //ensures the color swaps between black and white with every square
   bool whiteBlack;
   for (j=0;j<3;j++) {
     k = 0;
     for (i=0;i<3;i++) {
       whiteBlack = !whiteBlack;
+      //checks if there is a red piece in the nominal read and puts it in the correct square
       if (nominalRead [i][j] == 1) {
         display.fillCircle(piecePos[j][k],piecePos[j][k+1],4,whiteBlack);
         Serial.printf(" piece found");
       }
+      //checks if there is a blue piece in the nominal read and puts it in the correct square
       if (nominalRead [i][j] == 2) {
         display.fillTriangle(piecePos[j][k],piecePos[j][k+1]-4,piecePos[j][k]-4,piecePos[j][k+1]+4,piecePos[j][k]+4,piecePos[j][k+1]+4,whiteBlack);
         Serial.printf(" piece found");
       }
       Serial.printf("\nk = %i i = %i j = %i",k,i,j);
+      //this keeps x and y cordinates seperated in the array
       k+=2;
     }
   }
 }
-
-int pixelFill(int start, int end,int color) {
-
+//takes a start and end position and runs through a for loop until all pixels are on
+void pixelFill(int start, int end,int color) {
+  int neopixNum;
   for ( neopixNum = start; neopixNum < end; neopixNum ++) {
-
     neopix.setPixelColor(neopixNum, color);
     neopix.show();
-
-  }
-    
-  return end;
-
+  } 
 }
